1a98c531a78a214cec88964e5d6982df2fb45991
SupRMinecraftien <superminecraftien.levrai@gmail.com>
2024-10-19 01:03:09 +0200

    Render PDF pages as PNG images and extract date from images

--- main.py
+++ main.py
@@ -1,43 +1,49 @@
-import os
-from pick import pick
 from pdf2image import convert_from_path
+from pick import pick
+from PIL import Image
+import os, pytesseract, re
 
-def init():
-  title = "Please choose a file:"
-  warn = "Note: The files will end up with the id of product in the name."
-  dirList = os.listdir("./renders/pdf")
-  outputPath = "./renders/final/"
-  if dirList == []:
-    print("There are no files to render. Please add some files to the renders/pdf folder.")
-    exit()
-  return title, outputPath, warn, dirList
+def selector():
+  title = 'First, select a PDF file'
+  options = os.listdir('renders/pdf')
+  option, index = pick(options, title, indicator='=>', default_index=0)
+  return option
 
-def get_files(dirList, fileExtention):
-  files = []
-  for file in dirList:
-      if file.endswith(fileExtention) and not file.startswith('~$'):
-          files.append(file)
-  return files
-
-def pick_file(files, title, warn):
-  option, index = pick(files, title, indicator='=>', min_selection_count=1)
-  renderFileName = str(input(f'{warn}\nEnter the name of the file you want to render: '))
-  return option, index, renderFileName
-
-def render_pdf(file, outputPath, renderFileName):
-  print(f"🔄️ Rendering {file}")
-  pages = convert_from_path(f"./renders/pdf/{file}", 500)
+def render_file(file:str, prefix:str):
+  page_list = []
+  poppler_path = r'C:/Program Files/poppler-24.08.0/Library/bin'
+  pages = convert_from_path(prefix + file, 100, poppler_path=poppler_path)
   for i, page in enumerate(pages):
-    print(f"🔄️ Rendering page {i}")
-    page.save(f"{outputPath}{renderFileName}_{i}.jpg", 'JPEG')
-  print(f"🔄️ {file} has been rendered successfully!")
+    borders = [85, 1, 86, 2] # left, top, right, bottom
+    page = page.crop((borders[0], borders[1], page.width - borders[2], page.height - borders[3]))
+    page.save(f'renders/png/{file[:-4]}_{i}.png', 'PNG')
+    page_list.append(f'renders/png/{file[:-4]}_{i}.png')
+  return page_list
 
+def ocr_image(file:str):
+  image = Image.open(file)
+  pytesseract.pytesseract.tesseract_cmd = r'C:/Program Files/Tesseract-OCR/tesseract.exe'
+  
+  # Detect the text in the image
+  text = pytesseract.image_to_string(image)
+  
+  # Find date in text using regex
+  date_pattern = r'- [A-Z][a-z]{2} \d{2}/\d{2} -'
+  date_match = re.search(date_pattern, text)
+  if date_match:
+    date = date_match.group()
+    date = date[6:-2].replace('/', '-')
+    return date
+  return None
 
-if __name__ == '__main__':
-  title, outputPath, warn, dirList = init()
-  files = get_files(dirList, '.pdf')
-  option, index, renderFileName = pick_file(files, title, warn)
-  render_pdf(option, outputPath, renderFileName)
-  
-  
-  
+file = selector()
+prefix = 'renders/pdf/'
+
+page_list = render_file(file, prefix)
+
+for page in page_list:
+  print(page)
+  date = ocr_image(page)
+  print(date)
+
+# Ne marche pas sur les PDF contenant plusieurs pages...
